b2b0513463b63aaa809f6e629430139024a5d018
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2018-01-01 15:00:05 +0100

    Add var listener for buttons: Rock, Paper and Scissors

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,11 @@ var newGameBtn = document.getElementById('js-newGameButton');
 
 newGameBtn.addEventListener('click', newGame);
 
-//
+//Buttons  game Rock, Papre, Scissors
+var pickRock = document.getElementById('js-playerPick_rock'),
+    pickPaper = document.getElementById('js-playerPick_paper'),
+    pickScissors = document.getElementById('js-playerPick_scissors');
+
+pickRock.addEventListener('click', function() { playerPick('rock') });
+pickPaper.addEventListener('click', function() { playerPick('paper') });
+pickScissors.addEventListener('click', function() { playerPick('scissors') });
